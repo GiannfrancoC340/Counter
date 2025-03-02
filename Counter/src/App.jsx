37c47686635a9da709fae537fe45596630b3181f
GiannfrancoCrovetto <gcrovetto2021@fau.edu>
2025-03-02 13:06:42 -0500

    Finished the code

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Counter App</h1>
+      <p>You clicked the button {count} times!</p>
+      <button onClick={() => setCount(count+1)}>Click Me</button>
     </div>
   );
 }
