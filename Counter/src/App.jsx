010735d0e573ce97360641b66270ff3a4332deb1
GiannfrancoCrovetto <gcrovetto2021@fau.edu>
2025-03-01 20:11:29 -0500

    Started fresh with a new look in App.jsx

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -2,17 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    <div>
+      <h1>Counter App</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
